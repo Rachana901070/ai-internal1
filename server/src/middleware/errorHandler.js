79409386ddc669d80b1722045ee999b1718356cb
Rachana901070 <161238133+Rachana901070@users.noreply.github.com>
2025-10-15 12:55:42 +0530

    Revert "fix: set registration cookie"

--- server/src/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-export function errorHandler(err, req, res, next) {
-  const statusCode = res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
-  res.status(statusCode);
-  res.json({
-    success: false,
-    message: err.message || "Server error"
-  });
-}
